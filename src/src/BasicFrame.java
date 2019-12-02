45c387ac2196ade7e5475b3357e25d3073a236f6
31636 <31636@surface-why>
2019-12-03 00:04:34 +0800

    diyi

---
+++ src/src/BasicFrame.java
@@ -0,0 +1,12 @@
+package src;
+
+import javax.swing.JFrame;
+
+public abstract class BasicFrame extends JFrame{
+	public BasicFrame() {
+		// TODO Auto-generated constructor stub
+		super("Flappy Bird");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
