befb84e00188ba476f91ebf673384f7dd6a42f19
31636 <31636@surface-why>
2019-12-03 11:54:09 +0800

    添加了背景

--- src/src/BasicFrame.java
+++ src/src/BasicFrame.java
@@ -1,12 +1,34 @@
 package src;
 
-import javax.swing.JFrame;
+import java.awt.Container;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 
-public abstract class BasicFrame extends JFrame{
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class BasicFrame extends JFrame{
+	int score;
+	Container c;
+	JLabel back_lab;   //±³¾°Í¼Ïñ±êÇ©
 	public BasicFrame() {
 		// TODO Auto-generated constructor stub
 		super("Flappy Bird");
+		score=0;
+		this.setSize(720, 1280);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		c=this.getContentPane();
+		c.setLayout(null);
+		ImageIcon back_img=new ImageIcon("image/bg.jpg");
+		back_lab=new JLabel(back_img);
+		back_lab.setBounds(0, 0, back_img.getIconWidth(), back_img.getIconHeight());
+		c.add(back_lab);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {//test
+		new BasicFrame();
 	}
 }
