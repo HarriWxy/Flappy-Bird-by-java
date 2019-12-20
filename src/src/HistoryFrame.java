b93e1a0336f23880fadf2f3a6991ec39e61fe9c9
stephenlight <52495216+stephenlight@users.noreply.github.com>
2019-12-20 15:41:27 +0800

    滚动窗口的背景不知道为什么加不上去，还有滚动界面不知道为什么不能直接显示，要鼠标滑过才显示

--- src/src/HistoryFrame.java
+++ src/src/HistoryFrame.java
@@ -1,70 +1,89 @@
 package src;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 @SuppressWarnings("serial")
 public class HistoryFrame extends BasicFrame{//得分纪录界面
-	//改！！！！写记录的也改！！！！
+
+	private ArrayList<Integer> his_score = new ArrayList();
+	private Date date=new Date();
 	JButton back_but=new JButton("返回");
-	public HistoryFrame(int score, int hisscore) {
+
+	JList jList=new JList();
+
+	ImageIcon icon=new ImageIcon("image/bg.jpg");
+	JLabel labpic=new JLabel(icon);
+	public HistoryFrame() {
 		// TODO Auto-generated constructor stub
-		setTitle("FlappyBird History");
-		File hisfile=new File("flappybird hisscore.txt");
-		if(!hisfile.exists()){
+		File hisfile = new File("C:\\Users\\some people\\Desktop\\Flappy-Bird-by-java-degree2\\bin\\src\\flappybird hisscore.txt");
+		if (!hisfile.exists()) {
 			try {
-				PrintWriter output=new PrintWriter(hisfile);
-				score=0;//如果不存在历史记录文件，创建文件并初始化当前得分为0
-				hisscore=0;//初始化历史最高分为0
-				output.println(0);
-				output.println(0);
+				PrintWriter output = new PrintWriter(hisfile);
+				this.hisscore = 0;
+				output.println(this.date.toString() + " 0");
 				output.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+			} catch (FileNotFoundException var10) {
+				var10.printStackTrace();
 			}
-		}
-		else{
+		} else {
 			try {
-				Scanner input=new Scanner(hisfile);
-				hisscore=input.nextInt();
-				input.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+				Scanner input = new Scanner(hisfile);
+				while(input.hasNext()) {
+					this.his_score.add(Integer.valueOf(input.nextLine().substring(29)));
+				}
+				this.hisscore=his_score.get(0);
+			} catch (FileNotFoundException var11) {
+				var11.printStackTrace();
 			}
 		}
+
+		String[] listDate=new String[his_score.size()+1];
+		for(int i=0;i<his_score.size();i++){
+			listDate[i]="第"+(i+1)+": "+ his_score.get(i);
+		}
+		//创建列表
+		jList.setListData(listDate);
+		jList.setFont(new Font("宋体",Font.PLAIN,30));
+
+		JScrollPane scrollPane=new JScrollPane();//将列表添加到滚动框
+
+		scrollPane.setViewportView(jList);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);//设置需要时显示竖向滚动条
+		scrollPane.setWheelScrollingEnabled(true);//响应鼠标滚动
+		scrollPane.add(labpic);//想让滚动界面也有背景，但不知道为什么没弄成
+		scrollPane.setBorder(new EmptyBorder(5,5,5,5));//设置边框
 		
-				
-		this.setLayout(new FlowLayout(FlowLayout.LEFT,5,5));//设置顶级容器的布局为流式布局
-		Dimension dim = new Dimension(70,40);//设置大小
-		JLabel score_lb=new JLabel();
-		score_lb.setText("当前得分："+score);//当前得分标签
-		score_lb.setPreferredSize(dim);
-		JLabel hisscore_lb=new JLabel();
-		hisscore_lb.setText("历史最高："+hisscore);//历史最高得分标签
-		hisscore_lb.setPreferredSize(dim);
-		this.add(score_lb);
-		this.add(hisscore_lb);
-		
-		
+		this.setTitle("FlappyBird历史排行");//设置标题
+
+		this.setSize(500,500);//设置窗口大小
+		this.setLocationRelativeTo(null);//居中
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());//设置顶层为Border布局
+		this.add(scrollPane,BorderLayout.CENTER);//滚动界面位置
+
 		JPanel bottom_pan=new JPanel(new FlowLayout());
 		bottom_pan.setOpaque(false);
-		bottom_pan.setBounds(0,frame_height-10,frame_width,10);
 		bottom_pan.add(back_but);
-		c.add(bottom_pan);
-		
+		this.add(bottom_pan,BorderLayout.NORTH);//设置返回按钮在上中
+		this.validate();
+		this.setVisible(true);
 		listener();
-		this.setVisible(true);//设置可见
+
 	}
+
+
+
 	@Override
 		public void listener() {
 			// TODO Auto-generated method stub
@@ -79,6 +98,6 @@ public class HistoryFrame extends BasicFrame{//得分纪录界面
 			});
 		}
 	public static void main(String[] args) {
-		new HistoryFrame(1, 2);
+		new HistoryFrame();
 	} 
 }
